f5d06f5524bfdd1b098f80ea3aa2f7e2c1bb0d8d
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-07 22:45:31 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Lori Rothermel on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
